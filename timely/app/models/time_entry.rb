dd3641a56ab0b626370214adbc980b0a696a9211
Anna Petry <hello@annapetry.com>
2014-06-19 17:23:18 -0700

    Add callbacks to TimeEntry model, add balance and amount columns to Accounts table.

--- timely/app/models/time_entry.rb
+++ timely/app/models/time_entry.rb
@@ -3,7 +3,15 @@ class TimeEntry < AccountEntry
   
   validates :time, numericality: {greater_than: 0, less_than: 24}
 
-  def cost
-    time * 34
+  before_create :calculate_amount! # calling this before create ensures that even in the billing rates changes, the existing accounts won't be effected.  ONLY the newly created acounts will be effected.
+
+  def self.per_hour
+    34
+  end
+  
+  def calculate_amount!
+    self.amount = TimeEntry.per_hour * self.time
   end
 end
+
+
